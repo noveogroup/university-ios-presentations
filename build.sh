4ba15c670e71bac86d23e80e96290830b249bdc8
buildbot-ios <buildbot-ios@noveogroup.com>
2017-02-01 01:48:51 +0700

    updated build script

--- build.sh
+++ build.sh
@@ -1,17 +1,10 @@
-#generate lections
+#generate index
 python CreateLecture.py -s source/index.md -t templates/lecture_X.html
-python CreateLecture.py -s source/lecture_1.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_2.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_3.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_4.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_5_1.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_5_2.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_6.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_7.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_8.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_9.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_10.md -t templates/lecture_X.html -d html
-python CreateLecture.py -s source/lecture_11.md -t templates/lecture_X.html -d html
+
+#generate lections
+for i in source/*.md; do
+    python CreateLecture.py -s $i -t templates/lecture_X.html -d html
+done
 
 #copy resources
 mkdir public
