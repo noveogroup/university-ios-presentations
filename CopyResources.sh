115c2e2cd28ce6e31fb5e6309fe588b8ceac1722
buildbot-ios <buildbot-ios@noveogroup.com>
2017-02-06 00:47:35 +0700

    using travis CI

--- CopyResources.sh
+++ CopyResources.sh
@@ -1,6 +1,5 @@
 #copy resources
-mkdir public
-cp -r engine public/
-cp -r source public/
-cp -r html public/
-cp index.html public/
+mkdir out
+cp -r engine out/
+cp -r docs out/docs/
+cp index.html out/
