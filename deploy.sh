a8eb60806e179bbb73341a4a37bda8611c838e4f
buildbot-ios <buildbot-ios@noveogroup.com>
2017-02-06 09:21:43 +0700

    Github: fixed build script

--- deploy.sh
+++ deploy.sh
@@ -25,10 +25,11 @@ SHA=`git rev-parse --verify HEAD`
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
 # Clean out existing contents
 git ls-files | xargs rm -rf
+
+cd ..
 
 echo "root:"
 ls
